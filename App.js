911e462addafa7bb70830cca661e7af14ca17ca1
KRISHNA KUMAR MANDAL <118360906+coderkrishnabhai@users.noreply.github.com>
2024-06-22 03:02:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import AppNavigation from './src/navigation/AppNavigation'
+
+
+const App = () => {
+  return <AppNavigation/>;
+    
+  
+}
+
+export default App
